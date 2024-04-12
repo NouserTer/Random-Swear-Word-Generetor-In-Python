17ecea8dc8a9e8837bdcbc98ccad88fefb46df15
NouserTer <165575161+NouserTer@users.noreply.github.com>
2024-04-12 16:58:10 +0200

    Add files via upload

---
+++ randomWORDpicker.py
@@ -0,0 +1,8 @@
+import random
+
+random_words = ["Fuck", "shit", "son of bitch", "motherfucker", "Fuck you", "Fuck your mother", "KYS", "FUCKING KID", "Niger"]
+
+word = random.choice(random_words)
+
+print(word)
+
